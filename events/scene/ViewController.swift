ae5bdc61674bfe5e18ab0a7a03ffcfed28e85d78
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-10-23 15:27:07 -0300

    setup project

---
+++ events/scene/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  events
+//
+//  Created by Guilherme Colombini on 23/10/20.
+//  Copyright © 2020 Guilherme Colombini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+}
