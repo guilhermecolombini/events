5c49076b8e9386d255f6028d9c78c53ab76d5e69
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-10-24 19:40:42 -0300

    adding label class

--- events/view/Label.swift
+++ events/view/Label.swift
@@ -6,4 +6,75 @@
 //  Copyright © 2020 Guilherme Colombini. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Label: UILabel {
+    public enum Style {
+        case title, body
+    }
+    
+    var style: Style = .body {
+        didSet { updateStyle() }
+    }
+    
+    var customTextColor: UIColor? {
+        didSet { updateStyle() }
+    }
+    
+    override var text: String? {
+        set {
+            super.text = newValue
+            updateStyle()
+        }
+        get { return super.text }
+    }
+    
+    var shouldUpdateLayout: Bool = true
+    
+    // MARK: - Initialization
+    public convenience init(
+        text: String? = nil,
+        style: Style = .body,
+        customTextColor: UIColor? = nil,
+        textAlignment: NSTextAlignment = .left
+    ) {
+        self.init(frame: .zero)
+        shouldUpdateLayout = false
+        self.text = text
+        self.style = style
+        self.customTextColor = customTextColor
+        self.textAlignment = textAlignment
+        shouldUpdateLayout = true
+        customizeViews()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        customizeViews()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func customizeViews() {
+        guard shouldUpdateLayout else { return }
+        updateStyle()
+    }
+    
+    func updateStyle() {
+        switch (style) {
+        case .title:
+            textColor = Typography.title.color
+            font = Typography.title.font
+            
+        case .body:
+            textColor = Typography.body.color
+            font = Typography.body.font
+        }
+        
+        if let customTextColor = customTextColor {
+            textColor = customTextColor
+        }
+    }
+}
