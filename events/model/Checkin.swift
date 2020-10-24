c4a2d6ed081fcd1d321e330a4e23533319139c3b
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-10-24 19:03:53 -0300

    adding checkin struct

---
+++ events/model/Checkin.swift
@@ -0,0 +1,13 @@
+//
+//  Checkin.swift
+//  events
+//
+//  Created by Guilherme Colombini on 24/10/20.
+//  Copyright © 2020 Guilherme Colombini. All rights reserved.
+//
+
+import Foundation
+
+struct Checkin: Decodable {
+    let code: String
+}
