51209a4cf390dbe0e33ac87fda440effbcfb29c7
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-10-24 10:07:59 -0300

    adding events viewmodel

--- events/scene/events/EventViewModel.swift
+++ events/scene/events/EventViewModel.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import RxSwift
+
+struct EventViewModel {
+    let disposeBag = DisposeBag()
+    
+    let eventsPublish = PublishSubject<[Event]>()
+    
+    func fetchEvents() {
+        let service = Service()
+        service.request(endpoint: EventAPI.events)
+            .subscribe(onNext: { (result: Result<[Event], ServiceError>) in
+                switch (result) {
+                case .success(let events):
+                    self.eventsPublish.onNext(events)
+                    
+                case .failure(let error):
+                    print(error)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
