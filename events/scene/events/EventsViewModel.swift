c3e1b371e2508ce1950d654734a412748672ea6c
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-10-26 14:36:53 -0300

    update logic for new event cell view model

--- events/scene/events/EventsViewModel.swift
+++ events/scene/events/EventsViewModel.swift
@@ -9,23 +9,30 @@
 import Foundation
 import RxSwift
 
-struct EventsViewModel {
+class EventsViewModel {
     let disposeBag = DisposeBag()
+    private var events: [Event] = []
     
-    let eventsPublish = PublishSubject<[Event]>()
+    let eventsPublish = PublishSubject<[EventCellViewModel]>()
     
     func fetchEvents() {
         let service = Service()
         service.request(endpoint: EventAPI.events)
-            .subscribe(onNext: { (result: Result<[Event], ServiceError>) in
+            .subscribe(onNext: { [weak self] (result: Result<[Event], ServiceError>) in
+                guard let self = self else { return }
                 switch (result) {
                 case .success(let events):
-                    self.eventsPublish.onNext(events)
+                    self.events = events
+                    self.eventsPublish.onNext(events.map({ EventCellViewModel(event: $0) }))
                     
                 case .failure(let error):
                     print(error)
                 }
             })
             .disposed(by: disposeBag)
+    }
+    
+    func event(from row: Int) -> Event {
+        return events[row]
     }
 }
